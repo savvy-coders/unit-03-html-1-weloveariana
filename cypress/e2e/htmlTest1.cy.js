2e8460081adfa3a543757d46db17f95d5ce7428a
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-07-28 15:42:54 -0500

    add test

--- cypress/e2e/htmlTest1.cy.js
+++ cypress/e2e/htmlTest1.cy.js
@@ -1,6 +1,6 @@
 describe('My First HTML Project page', () => {
   beforeEach(() => {
-    cy.visit('/index.html');
+    cy.visit('/Unit3Solution.html');
   });
 
   it('has the correct title', () => {
