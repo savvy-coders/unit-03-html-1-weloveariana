f77bbe11c2a6331217414bb3efdc456211412295
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-09-27 12:09:44 -0500

    update tests to decrease test flake

--- cypress/e2e/htmlTest4.cy.js
+++ cypress/e2e/htmlTest4.cy.js
@@ -1,10 +1,9 @@
 describe('My First HTML Project page - Paragraph', () => {
-    beforeEach(() => {
-      cy.visit('/index.html');
-    });
-  
-    it('displays a p with the correct text', () => {
-      cy.get('p').should('have.text', 'This is my first coding project using HTML!');
-    });
+  beforeEach(() => {
+    cy.visit('/index.html');
   });
-  
+
+  it('displays a p with the correct text', () => {
+    cy.get('p').contains('This is my first coding project using HTML!').should('exist');
+  });
+});
